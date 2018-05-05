9c10d72c2f532edbc5fa744fd912adfc90451c94
Chris Savoie <chris@chrissavoie.com>
2018-05-05 01:02:24 -0700

    Fix linux build script to only run unit tests after successful compile.

--- build_and_run.sh
+++ build_and_run.sh
@@ -13,9 +13,11 @@ fi
 
 pushd projects
 make -f simpletestsolution_linux_${toolset}.make config=${config} ${extra}
+exitcode=$?
 popd
 
-if [ "$extra" != "clean" ]; then
-    ./.temp/bin/simpletestproject_linux_${toolset}_${config}
+if [ ${exitcode} = 0 ]; then
+    if [ "$extra" != "clean" ]; then
+        ./.temp/bin/simpletestproject_linux_${toolset}_${config}
+    fi
 fi
-
