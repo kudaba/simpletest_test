c919e3ba0b6f22648b65ad6f99afe02415622ad5
Chris Savoie <chris@chrissavoie.com>
2018-05-05 01:03:51 -0700

    Add an example for testing code that is expected to crash. It only works on linux since it uses fiork. Update to latest version

--- src/exampletests/threadingtests.cpp
+++ src/exampletests/threadingtests.cpp
@@ -65,3 +65,50 @@ DEFINE_TEST(ThreadingTest)
 
 	TEST_EQ(locThreadIndex, 4);
 }
+
+//---------------------------------------------------------------------------------
+// Example of testing code that is expected to crash. This isn't very portable
+// so it's not part of the main system.
+//---------------------------------------------------------------------------------
+
+#if !defined(WIN64)
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+static void Crash()
+{
+	__builtin_trap();
+}
+
+#define UNIQUE(a) TOK(a, __LINE__)
+
+#define TEST_CRASH(cond) \
+pid_t UNIQUE(pid) = fork(); \
+if (UNIQUE(pid) == 0) { cond; _exit(0); } else { \
+int UNIQUE(status) = 0; wait(&UNIQUE(status)); \
+if (!WIFSIGNALED(UNIQUE(status))) TEST_MESSAGE(0 && STR(cond), "Expected %s to crash, but it ran successfully.", STR(cond) ); }
+
+DEFINE_TEST(ForkTest)
+{
+	pid_t pid = fork();
+	
+	if (pid == 0)
+	{
+		Crash();
+		_exit(1);
+	}
+	else
+	{
+		int status = 0;
+		pid_t doneid = wait(&status);
+		TEST_EQ(pid, doneid);
+
+		TEST_EQ(WIFEXITED(status), false);
+		TEST(WIFSIGNALED(status));
+	}
+
+	TEST_CRASH(Crash());
+}
+#endif
